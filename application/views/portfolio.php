c61d0399fdf245e32d81228fec2110591b867438
jcsantos23 <jcsantos23@gmail.com>
2016-02-09 14:17:08 -0800

    Coverted hmtl to php and kept only the contents

---
+++ application/views/portfolio.php
@@ -0,0 +1,16 @@
+<div class="player-select">
+    <label for="player">Player</label>
+    <select>
+        <option value="player1">Mikey</option>
+        <option value="player2">Donald</option>
+        <option value="player3">George</option>
+        <option value="player4">Henry</option>
+    </select>
+</div>
+<div class="alone"></div>
+<div class="left-panel">
+    <label for="recent-act">Player's Recent Activity</label>
+</div>
+<div class="right-panel">
+    <label for="holdings">Current Holdings</label>
+</div>
