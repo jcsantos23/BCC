54683e8fc273e8d157b545b77271319dc65936be
jcsantos23 <jcsantos23@gmail.com>
2016-02-09 14:18:12 -0800

    Created application controller to handle our views

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * core/MY_Controller.php
+ *
+ * Default application controller
+ */
+class Application extends CI_Controller {
+
+    protected $data = array();      // parameters for view components
+    protected $id;    // identifier for our content
+    protected $choices = array(// our menu navbar
+        'Home' => '/', 'Porfolio' => '/portfolio', 'Assembly' => '/aseembly');
+
+    /**
+     * Constructor.
+     * Establish view parameters & load common helpers
+     */
+    function __construct() {
+        parent::__construct();
+        $this->data = array();
+        $this->data['pagetitle'] = 'Bot Card Collector';
+    }
+
+    /**
+     * Render this page
+     */
+    function render() {
+        $this->data['menubar'] = build_menu_bar($this->choices);
+        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+        $this->data['data'] = &$this->data;
+        $this->parser->parse('_template', $this->data);
+    }
+
+}
+
+/* End of file MY_Controller.php */
+/* Location: application/core/MY_Controller.php */
