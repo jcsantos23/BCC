71ac113fec4474b745cdc2fbadf657acfbcf90f9
jcsantos23 <jcsantos23@gmail.com>
2016-02-11 17:09:13 -0800

    Modified code

--- application/controllers/Assembly.php
+++ application/controllers/Assembly.php
@@ -5,6 +5,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Assembly extends Application {
 
     public function index() {
+        
+        $records = $this->collections->all();
+        
+        foreach ($records as $row){
+           echo $row->Token;
+        }
+          
         $this->data['pagebody'] = 'assembly';
         $this->render();
     }
