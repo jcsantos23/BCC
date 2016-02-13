af036190257a03bfc30dd526a2cbc97a00bfd09f
johna <johna@DESKTOP-U77VMO7>
2016-02-12 16:20:19 -0800

    codes model created

---
+++ application/models/Codes.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Codes
+ *
+ * @author JL
+ */
+class Codes {
+    function __construct() {
+        //parameter: 'tableName','orderByTableColumn
+        parent::__construct('codes','quantity');
+    }
+}
