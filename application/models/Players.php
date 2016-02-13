dd385dd2a71062d06610581511643312209a6376
johna <johna@DESKTOP-U77VMO7>
2016-02-12 16:21:06 -0800

    Players.php model created

---
+++ application/models/Players.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Players
+ *
+ * @author JL
+ */
+class Players extends MY_Model{
+    function __construct() {
+        //parameter: 'tableName','orderByTableColumn
+        parent::__construct('players','peanuts');
+    }
+}
