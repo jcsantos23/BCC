3283d3ef9f5a2982a26f8a69634455a97d6584cf
johna <johna@DESKTOP-U77VMO7>
2016-02-12 16:21:29 -0800

    Transactions.php model created

---
+++ application/models/Transactions.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Transactions
+ *
+ * @author JL
+ */
+class Transactions extends MY_Model{
+    function __construct() {
+        //parameter: 'tableName','orderByTableColumn
+        parent::__construct('transactions','DateTime');
+    }
+}
