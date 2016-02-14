c0087c1d5142c5ddb85d6bae8849934d06f130fd
jcsantos23 <jcsantos23@gmail.com>
2016-02-14 13:15:22 -0800

    Modified content

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,14 +1,6 @@
 <div class="left-panel">
-    <table class="table-left-game">
-        <tr>
-            <th>Game Status</th>
-            <th>Game Summary</th>
-        </tr>
-        <tr>
-            <td>12</td>
-            <td>12</td>
-        </tr>
-    </table>
+    <label class="summary">Game Summary - Known Bots</label>
+    {bot}
 </div>
 <div class="right-panel">
     {wtable}
