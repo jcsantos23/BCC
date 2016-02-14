e1f31e1fdf43bd0fe37d47eb388d3f59789e052e
johna <johna@DESKTOP-U77VMO7>
2016-02-14 15:47:00 -0800

    Modified to show all data

--- application/views/portfolio.php
+++ application/views/portfolio.php
@@ -1,16 +1,21 @@
-<div class="player-select">
-    <label for="player">Player</label>
-    <select>
-        <option value="player1">Mikey</option>
-        <option value="player2">Donald</option>
-        <option value="player3">George</option>
-        <option value="player4">Henry</option>
-    </select>
-</div>
+<form method="post">
+    <div class="player-select">
+        <label for="player">Player</label>
+        <select name = pname>
+            <option value="player1">Mickey</option>
+            <option value="player2">Donald</option> 
+            <option value="player3">George</option>
+            <option value="player4">Henry</option>
+        </select>
+    </div>
+</form> 
 <div class="alone"></div>
 <div class="left-panel">
     <label for="recent-act">Player's Recent Activity</label>
+    {ptable}
 </div>
 <div class="right-panel">
     <label for="holdings">Current Holdings</label>
+    {ptable2}
 </div>
+<input type="submit" value="Submit" name="formSubmit"> 
