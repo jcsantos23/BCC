32a19ad3a89f37702718cfd319c493d80ac8d349
johna <johna@DESKTOP-U77VMO7>
2016-02-15 15:23:17 -0800

    Updated Portfolio

--- application/models/Collections.php
+++ application/models/Collections.php
@@ -1,21 +1,25 @@
 <?php
 
 class Collections extends MY_Model {
+
     // constructor
     function __construct() {
         //parameter: 'tableName','pk'
         parent::__construct('collections', 'Token', 'Player', 'Piece', 'Datetime');
     }
-    
+
     function get_cards($current_player) {
         $collection = $this->db->get_where('collections', array('Player' => $current_player))->result_array();
         return $collection;
     }
-    
+
     function sort_cards($collection) {
         $card_array = array("elevena0" => 0, "elevena1" => 0, "elevena2" => 0,
                             "elevenb0" => 0, "elevenb1" => 0, "elevenb2" => 0,
-                            "elevenc0" => 0, "elevenc1" => 0, "elevenc2" => 0);
+                            "elevenc0" => 0, "elevenc1" => 0, "elevenc2" => 0,
+                            "thirteenc0" => 0, "thirteenc1" => 0, "thirteenc2" => 0,
+                            "thirteend0" => 0, "thirteend1" => 0, "thirteend2" => 0,
+                            "twentysixh0" => 0, "twentysixh1" => 0, "twentysixh2" => 0);
 
         if (!empty($collection)) {
             foreach ($collection as $card) {
@@ -79,4 +83,5 @@ class Collections extends MY_Model {
             return $card_array;
         }
     }
+
 }
