ea800fd241db22217aadd585d4c3704f9692cfaa
jcsantos23 <jcsantos23@gmail.com>
2016-04-15 20:10:33 -0800

    Updated

---
+++ application/controllers/Registration.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Assembly page to assemble the bot.
+ * 
+ * ------------------------------------------------------------------------
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Registration extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+
+        $this->data['error-message'] = "";
+        $this->signup();
+        $this->data['pagebody'] = 'registration';
+
+        //puts it into the view
+        $this->render();
+    }
+
+    public function signup() {
+
+        if (!is_null($this->input->post('signup'))) {
+            if (!empty($this->input->post('username')) && !empty($this->input->post('password'))) {
+
+                if ($this->Players->exists($this->input->post('username'))) {
+                    $this->data['error-message'] = "Username exists";
+                } else {
+                    $data = array(
+                        'Player' => $this->input->post('username'),
+                        'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT)
+                    );
+                    $this->Players->add($data);
+
+                    $this->session->response = "Registration Complete!! Welcome, ".$this->input->post('username');
+                    $this->session->username = $this->input->post('username');
+                    
+                    redirect('');
+                }
+            } else {
+                $this->data['error-message'] = "Please enter a username and/or password";
+            }
+        }
+    }
+
+}
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -26,19 +26,30 @@ class Application extends CI_Controller {
      * Render this page
      */
     function render() {
-        
-        
-        $cred = $this->session->userdata['username'];
-        if ($cred !== '') {
-            $this->data['username'] = $this->session->userdata['username'];
-            $this->data['credential'] = $this->parser->parse('signout', $this->data, true);
-        } else {
-            $this->data['credential'] = $this->parser->parse('signin', $this->data, true);
-        }
-        $this->data['username'] = $this->session->userdata['username'];
-        
+
+
+//        $cred = $this->session->userdata['username'];
+//        if ($cred !== '') {
+//            $this->data['username'] = $this->session->userdata['username'];
+//            $this->data['credential'] = $this->parser->parse('signout', $this->data, true);
+//        } else {  
+//            $this->data['credential'] = $this->parser->parse('signin', $this->data, true);
+//        }
+//        $this->data['username'] = $this->session->userdata['username'];
         //$this->data['menubar'] = build_menu_bar($this->choices);
+        $this->data['display'] = "";
+        if (!empty($this->session->userdata('response'))) {
+            $this->data['display'] = $this->session->userdata('response');
+            $this->session->unset_userdata('response');
+        }
+
         $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+        
+        if (!empty($this->session->userdata('username'))) {
+            $this->data['navigation'] = $this->parser->parse('navigation_user', array(), true);
+        } else {
+            $this->data['navigation'] = $this->parser->parse('navigation_def', array(), true);
+        }
         $this->data['data'] = &$this->data;
         $this->parser->parse('template', $this->data);
     }
