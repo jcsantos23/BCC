d0ea5a62ad27766a5585077bed52976bad4677d2
jcsantos23 <jcsantos23@gmail.com>
2016-04-16 17:57:10 -0800

    Modified template.php view for appropriate nav bar

--- application/views/template.php
+++ application/views/template.php
@@ -9,20 +9,8 @@
     </head>
     <body>
         <div id="wrapper">
-            <div id="header">
-                <!-- navigation menu -->
-                <span class="myhead">Bot Card Collector</span>
-                <span class="nav">
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/portfolio">Portfolio</a></li>
-                        <li><a href="/assembly">Assembly</a></li>
-                        <li><a href="/signin">Login</a></li>
-                        
-                    </ul>
-                </span>
-            </div>
-            <div class="alone"></div>
+            {navigation}
+            <div class="alone">{display}</div>
             <!-- populates the body with content -->
             <div id="content">
                 {content}
