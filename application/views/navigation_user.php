22664efc113758411a8bb834cba0782cb4777a45
jcsantos23 <jcsantos23@gmail.com>
2016-04-16 16:42:47 -0800

    Added appropriate files to fix conflicting merge

---
+++ application/views/navigation_user.php
@@ -0,0 +1,18 @@
+<!-- Default Navigation Menu for Authenticated users -->
+
+<div id="header">
+    <!-- navigation menu -->
+    <span class="myhead">Bot Card Collector</span>
+    <span class="nav">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/portfolio">Portfolio</a></li>
+            <li><a href="/assembly">Assembly</a></li>
+            <li>
+                <form method="post">
+                    <input type="submit" name="logout" value="Logout"/>
+                </form>
+            </li>
+        </ul>
+    </span>
+</div>
