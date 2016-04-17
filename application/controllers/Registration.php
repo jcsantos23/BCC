e8c4373f9a218d8dcf37344ee79f904823e4f436
jcsantos23 <jcsantos23@gmail.com>
2016-04-16 17:55:39 -0800

    Added Registration.php Controller

--- application/controllers/Registration.php
+++ application/controllers/Registration.php
@@ -1,37 +1,57 @@
 <?php
+
 /**
- * Registration page to register a user.
+ * Assembly page to assemble the bot.
  * 
  * ------------------------------------------------------------------------
  */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 class Registration extends Application {
-    
+
     function __construct() {
         parent::__construct();
     }
-    
-    function index() {
-        $this->data['pagebody'] = 'registration'; // this is the view we want shown
+
+    public function index() {
+
+        $this->data['error-message'] = "";
+        $this->signup();
+        $this->data['pagebody'] = 'registration';
+
+        //puts it into the view
         $this->render();
     }
-    
-    function register(){
-        $username = $this->input->post('username');
-        $password = $this->input->post('password');
-        $result = $this->user->checkUser($username);
-        if($result[0] != null){
-            
-        }else{
-            $data = array(
-                'player' => $username,
-                'Peanuts' => '100',
-                'Password' => $password,
-                'Role' => 'user',
-                'Avatar' => '',
-            );
-            $this->user->insertUser($data);
-        }
-        redirect('/', 'refresh');
-    }
-}
 
+    public function signup() {
+
+        if (!is_null($this->input->post('signup'))) {
+            if (!empty($this->input->post('username')) && !empty($this->input->post('password'))) {
+
+                if ($this->Players->exists($this->input->post('username'))) {
+                    $this->data['error-message'] = "Username exists";
+                } else {
+                    $data = array(
+                        'Player' => $this->input->post('username'),
+                        'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                        
+                        //set Peanuts value initially to 0
+                        'Peanuts' => "0",
+                        
+                        //set the default role to regular player which is 0
+                        'role' => "0"
+                    );
+                    $this->Players->add($data);
+
+                    $this->session->response = "Registration Complete!! Welcome, ".$this->input->post('username');
+                    $this->session->username = $this->input->post('username');
+                    
+                    redirect('');
+                }
+            } else {
+                $this->data['error-message'] = "Please enter a username and/or password";
+            }
+        }
+    }
+
+}
