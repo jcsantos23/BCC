f60153e5078265a101772c0312520c04eaf01ae1
jcsantos23 <jcsantos23@gmail.com>
2016-04-16 17:10:02 -0700

    Delete signin.php

--- application/views/signin.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" type="text/css" href="/css/style.css">
-
-<!-- login box for players to sign in, currently only needs a username -->
-<form action="/login/login" method="post">
-    Username: <input type="text" name="username" style="width:50px;"/>
-    <input type="submit" value="Login" />
-</form>
