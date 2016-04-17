1ba29b4f49787e3f2550203f74c0ba10ea3ace46
jcsantos23 <jcsantos23@gmail.com>
2016-04-16 17:56:07 -0800

    Modified MY_Controller to parse appropriate views

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -10,7 +10,7 @@ class Application extends CI_Controller {
     protected $data = array();      // parameters for view components
     protected $id;    // identifier for our content
     protected $choices = array(// our menu navbar
-        'Home' => '/', 'Portfolio' => '/portfolio', 'Assembly' => '/assembly', 'Login'=> '/signin');
+        'Home' => '/', 'Portfolio' => '/portfolio', 'Assembly' => '/assembly', 'Login' => '/signin');
 
     /**
      * Constructor.
@@ -21,30 +21,47 @@ class Application extends CI_Controller {
         $this->data = array();
         $this->data['pagetitle'] = 'Bot Card Collector';
         $this->errors = array();
-        $this->data['pageTitle'] = 'welcome';  
+        $this->data['pageTitle'] = 'welcome';
     }
 
     /**
      * Render this page
      */
     function render() {
-        if( $this->session->userdata['username'] == ''){
-             //$this->data['logincred'] = $this->parser->parse('signin', $this->data, true);
-        }else{
-            $this->data['username'] = $this->session->userdata['username'];
-            $this->data['logincred'] = $this->parser->parse('signout', $this->data, true);
+        $this->data['display'] = "";
+        if (!empty($this->session->userdata('response'))) {
+            $this->data['display'] = $this->session->userdata('response');
+            $this->session->unset_userdata('response');
         }
-        //$this->data['menubar'] = $this->parser->parse('_menubar', $this->config->item('menu_choices'), true);
+
         $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
-        
-        // finally, build the browser page!
-        $this->data['username'] = $this->session->userdata['username'];
+
+        if (!empty($this->session->userdata('username'))) {
+            if ($this->session->userdata('role') == 99) {
+                $this->data['navigation'] = $this->parser->parse('navigation_admin', array(), true);
+                $this->logout();
+            } else {
+                $this->data['navigation'] = $this->parser->parse('navigation_user', array(), true);
+                $this->logout();
+            }
+        } else {
+            $this->data['navigation'] = $this->parser->parse('navigation_def', array(), true);
+        }
         $this->data['data'] = &$this->data;
         $this->parser->parse('template', $this->data);
     }
-        
+
+    function logout() {
+
+        //Logout button onClick
+        if (!is_null($this->input->post('logout'))) {
+            $this->session->sess_destroy();
+            redirect('');
+        }
     }
 
-    /* End of file MY_Controller.php */
+}
+
+/* End of file MY_Controller.php */
     /* Location: application/core/MY_Controller.php */
     
