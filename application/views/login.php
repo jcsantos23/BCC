e35ac697a944282bb4caf229d9b19e4ef251b3ca
jcsantos23 <jcsantos23@gmail.com>
2016-04-16 17:57:25 -0800

    Added login.php view

--- application/views/login.php
+++ application/views/login.php
@@ -1,1 +1,9 @@
-<!-- placeholder page for login/logout to work properly -->
+<form method="post">
+    <label for="username">Username: </label>
+    <input type="text" name="username"/><br/><br/>
+    <label for="password">Password: </label>
+    <input type="password" name="password"/><br/><br/>
+    <input type="submit" name="login" value="LOGIN" />
+    <button><a href="/registration">Sign-up</a></button><br/>
+    {error-message}
+</form>
